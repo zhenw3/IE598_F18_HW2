5b6f6e2b7829fcd568a091c1c3a7fce837ccfa77
zhenw3 <42774039+zhenw3@users.noreply.github.com>
2018-09-10 05:19:40 +0800

    made some changes to the code

--- knn_k.py
+++ knn_k.py
@@ -11,7 +11,48 @@ from matplotlib.colors import ListedColormap
 import matplotlib.pyplot as plt
 
 
+#write plot decision regions function
+def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
 
+    # setup marker generator and color map
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    # plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.3, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0], 
+                    y=X[y == cl, 1],
+                    alpha=0.8, 
+                    c=colors[idx],
+                    marker=markers[idx], 
+                    label=cl, 
+                    edgecolor='black')
+
+    # highlight test samples
+    if test_idx:
+        # plot all samples
+        X_test, y_test = X[test_idx, :], y[test_idx]
+
+        plt.scatter(X_test[:, 0],
+                    X_test[:, 1],
+                    c='',
+                    edgecolor='black',
+                    alpha=1.0,
+                    linewidth=1,
+                    marker='o',
+                    s=100, 
+                    label='test set')
 
 #import data
 iris = datasets.load_iris()
@@ -33,10 +74,51 @@ sc = StandardScaler()
 sc.fit(X_train)
 X_train_std = sc.transform(X_train)
 X_test_std = sc.transform(X_test)
+X_combined_std = np.vstack((X_train_std, X_test_std))
+y_combined = np.hstack((y_train, y_test))
+
+#train decision tree model and plot
+from sklearn.tree import DecisionTreeClassifier
+
+tree = DecisionTreeClassifier(criterion='gini', 
+                              max_depth=4, 
+                              random_state=1)
+tree.fit(X_train, y_train)
+
+X_combined = np.vstack((X_train, X_test))
+y_combined = np.hstack((y_train, y_test))
+plot_decision_regions(X_combined, y_combined, 
+                      classifier=tree, test_idx=range(105, 150))
+
+plt.xlabel('petal length [cm]')
+plt.ylabel('petal width [cm]')
+plt.legend(loc='upper left')
+plt.title('decision tree')
+plt.tight_layout()
+#plt.savefig('images/03_20.png', dpi=300)
+plt.show()
+
+
+#train a knn model and plot
+from sklearn.neighbors import KNeighborsClassifier
+knn = KNeighborsClassifier(n_neighbors=5, 
+                           p=2, 
+                           metric='minkowski')
+knn.fit(X_train_std, y_train)
+
+plot_decision_regions(X_combined_std, y_combined, 
+                      classifier=knn, test_idx=range(105, 150))
+
+plt.xlabel('petal length [standardized]')
+plt.ylabel('petal width [standardized]')
+plt.legend(loc='upper left')
+plt.title('knn')
+plt.tight_layout()
+#plt.savefig('images/03_24.png', dpi=300)
+plt.show()
 
 
 #trained Knn model k times
-from sklearn.neighbors import KNeighborsClassifier
 from sklearn import metrics
 
 k_range=range(1,26)
@@ -53,11 +135,11 @@ plt.plot(k_range,scores)
 
 plt.xlabel('k',fontsize=15)
 plt.ylabel('scores',fontsize=15)
-plt.legend(loc='upper left')
+plt.title('k versus scores')
 plt.tight_layout()
 #plt.savefig('images/03_24.png', dpi=300)
 plt.show()
 
-print("My name is Zhen")
+print("My name is Zhen Wang")
 print("My NetID is: zhenw3")
 print("I hereby certify that I have read the University policy on Academic Integrity and that I am not in violation.")
